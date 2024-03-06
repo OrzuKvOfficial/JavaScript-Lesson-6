eb8ba85baa0a62a5f96997fe34ff7f82e9cc77c8
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-03-05 20:43:53 -0500

    Without any change

--- server.js
+++ server.js
@@ -87,3 +87,16 @@ var data = [
         resultDiv.innerHTML = "Bunday ID topilmadi";
     }
   }
+  function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+  }
+
+  function clearDisplay() {
+    document.getElementById("display").value = '';
+  }
+
+  function calculateResult() {
+    let expression = document.getElementById("display").value;
+    let result = eval(expression);
+    document.getElementById("display").value = result;
+  }
